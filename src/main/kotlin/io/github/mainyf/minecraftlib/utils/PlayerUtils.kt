7d08c980ecee1689a0d3e6ba082568a501bb86fd
Mainyf <1850343382@qq.com>
2019-04-21 13:09:35 +0800

    optimize armor function

--- src/main/kotlin/io/github/mainyf/minecraftlib/utils/PlayerUtils.kt
+++ src/main/kotlin/io/github/mainyf/minecraftlib/utils/PlayerUtils.kt
@@ -15,11 +15,11 @@ object PlayerUtils {
     }
 
     fun getPlayerMarkRangeValue(items: Collection<ItemStack>, mark: String): Double? {
-        return items.reduce({ prev: Double?, item: ItemStack, i: Int ->
+        return items.reduce { prev: Double?, item: ItemStack, i: Int ->
             val range = LoreUtils.getItemRangeValueDouble(item, mark) ?: return@reduce prev
             val value = range.randomValue()
             if(prev == null) value else prev + value
-        })
+        }
     }
 
     fun getPlayerMarkRangeDouble(entity: LivingEntity, mark: String): DoubleValueRange? {
@@ -31,7 +31,7 @@ object PlayerUtils {
     }
 
     fun getPlayerMarkRangeDouble(items: Collection<ItemStack>, mark: String): DoubleValueRange? {
-        return items.reduce({ prev: DoubleValueRange?, item: ItemStack, i: Int ->
+        return items.reduce(fn = { prev: DoubleValueRange?, item: ItemStack, i: Int ->
             val range = LoreUtils.getItemRangeValueDouble(item, mark) ?: return@reduce prev
             if(prev == null) range else {
                 prev.mergeRange(range)
@@ -49,13 +49,13 @@ object PlayerUtils {
     }
 
     fun getPlayerMarkRangeInt(items: Collection<ItemStack>, mark: String): IntValueRange? {
-        return items.reduce({ prev: IntValueRange?, item: ItemStack, i: Int ->
+        return items.reduce { prev: IntValueRange?, item: ItemStack, i: Int ->
             val range = LoreUtils.getItemRangeValueInt(item, mark) ?: return@reduce prev
             if(prev == null) range else {
                 prev.mergeRange(range)
                 prev
             }
-        })
+        }
     }
 
     fun getPlayerMarkValue(entity: LivingEntity, mark: String): Double? {
@@ -67,10 +67,10 @@ object PlayerUtils {
     }
 
     fun getPlayerMarkValue(items: Collection<ItemStack>, mark: String): Double? {
-        return items.reduce({ prev: Double?, item: ItemStack, i: Int ->
+        return items.reduce { prev: Double?, item: ItemStack, i: Int ->
             val value = LoreUtils.getItemValue(item, mark) ?: return@reduce prev
             if(prev == null) value else prev + value
-        })
+        }
     }
 
     fun getPlayerMarkPercentValue(entity: LivingEntity, mark: String): Double? {
@@ -82,10 +82,10 @@ object PlayerUtils {
     }
 
     fun getPlayerMarkPercentValue(items: Collection<ItemStack>, mark: String): Double? {
-        return items.reduce({ prev: Double?, item: ItemStack, i: Int ->
+        return items.reduce { prev: Double?, item: ItemStack, i: Int ->
             val value = LoreUtils.getItemPercent(item, mark) ?: return@reduce prev
             if(prev == null) value else prev + value
-        })
+        }
     }
 
 }
